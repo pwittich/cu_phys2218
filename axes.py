e971f7ec25c1166cccbaa036790b74bf3cdf0eb1
Peter Wittich <wittich@cornell.edu>
2024-09-03 09:52:04 -0400

    add existing files

---
+++ axes.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+from matplotlib.axes import Axes
+import matplotlib.pyplot as plt
+
+def axes( ax: Axes = 0 ) -> None:
+    """Set up axes and plot in a way that is typically used in physics class"""
+    # if no ax passed in, use the default 
+    if ( ax == 0 ):
+        ax = plt.gca()
+    # set the x-spine
+    ax.spines['left'].set_position('zero')
+
+    # turn off the right spine/ticks
+    ax.spines['right'].set_color('none')
+    ax.yaxis.tick_left()
+
+    # set the y-spine
+    ax.spines['bottom'].set_position('zero')
+
+    # turn off the top spine/ticks
+    ax.spines['top'].set_color('none')
+    ax.xaxis.tick_bottom()
+
+
+    # Draw arrows (as black triangles: ">k"/"^k") at the end of the axes.  In each
+    # case, one of the coordinates (0) is a data coordinate (i.e., y = 0 or x = 0,
+    # respectively) and the other one (1) is an axes coordinate (i.e., at the very
+    # right/top of the axes).  Also, disable clipping (clip_on=False) as the marker
+    # actually spills out of the axes.
+    ax.plot(1, 0, ">k", transform=ax.get_yaxis_transform(), clip_on=False)
+    ax.plot(0, 1, "^k", transform=ax.get_xaxis_transform(), clip_on=False)
+    # turn on a grid
+    ax.grid(True)
